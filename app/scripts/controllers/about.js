4011f5672120b4c0d367acfd6b6d77561860cd9e
yago11 <yashagoonki@gmail.com>
2015-11-19 16:13:54 +0200

    first atom commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stingrayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the stingrayApp
+ */
+angular.module('stingrayApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
