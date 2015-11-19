0953f4a6fa8981d101a60aa058a1e861e068bee0
lstrichashl <lstrichashl@gmail.com>
2015-11-19 16:56:08 +0200

    arrange material
    make material work

--- app/app.js
+++ app/app.js
@@ -13,20 +13,11 @@ angular
     'ngAnimate',
     'ngResource',
     'ngRoute',
-    'ngSanitize'
+    'ngSanitize',
+    'ngMaterial'
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
       .otherwise({
         redirectTo: '/'
       });
