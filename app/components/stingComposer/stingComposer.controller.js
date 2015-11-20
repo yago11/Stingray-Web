f8e62ebbcd8a4e7aeaf2b7e5a0727f7bfcea79e8
lstrichashl <lstrichashl@gmail.com>
2015-11-20 06:13:10 +0200

    sting composer continued

--- app/components/stingComposer/stingComposer.controller.js
+++ app/components/stingComposer/stingComposer.controller.js
@@ -11,7 +11,6 @@
   function stingComposerController() {
     var vm = this;
     vm.title = 'stingComposerController';
-    vm.geocodeAddress = geocodeAddress;
     vm.geocode = new google.maps.Geocoder();
     vm.composeSting = composeSting;
 
@@ -24,14 +23,7 @@
     }
 
     function composeSting(sting){
-
-      var user = {
-        id: 1,
-        name: 'sapir',
-        facebook_user_id: 1,
-        user_type: 'soldier'
-      };
-      geocoder.geocode({'address': address}, function(results, status) {
+      vm.geocoder.geocode({'address': sting.address}, function(results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
           var a = results[0].geometry.location;
           $http({
@@ -41,15 +33,8 @@
               'AnonymousToken': '23fd826e-e4d1-41a6-a91c-45e6ab6d213f'
             },
             data: {
-              User: user,
-              Place: {
-                User: user,
-                id: 11,
-                name: 'palaspel',
-                description: 'palapel',
-                langtitude: a[0],
-                longtitude: a[1]
-              },
+              User: 1,
+              Place: sting.name,
               price: sting.price,
               description: sting.description
             }
@@ -57,10 +42,6 @@
         } else {
         }
       });
-    }
-
-    function geocodeAddress(geocoder, address) {
-
     }
   }
 
--- app/components/stingComposer/stingComposer.directive.js
+++ app/components/stingComposer/stingComposer.directive.js
@@ -11,10 +11,11 @@
   function stingComposer() {
     return {
       restrict: 'E',
+      templateUrl: 'components/stingComposer/stingComposer.html',
       scope: {},
       controller: 'stingComposerController',
       controllerAs: 'vm',
-      bindToController: true,
+      bindToController: true
     };
   }
 })();
